e51fac5adab2fbf45a4163269886c424e61463dc
Sheldon Pasciak <sheldon@pasciak.com>
2024-07-31 13:43:25 -0600

    Misc

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+// index.js or App.js
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <body>
+          Now is the time to test a font!
+          <p>Test test test</p>
+          <div className="test">Test</div>
+        </body>
+      </div>
+      <div className="container bg-primary">
+        <div className="row">
+          <div className="col">
+            <p>Sebastian</p>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col bg-success">Left Column</div>
+          <div className="col bg-success">Right Column</div>
+        </div>
+      </div>
+    </>
   );
 }
 
