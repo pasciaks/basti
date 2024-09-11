c29bcc49f2d6a1bebf2c8976e955bc1bc1dc2ad8
Sheldon Pasciak <sheldon@pasciak.com>
2024-09-11 13:36:40 -0600

    Misc start with bootstrap collapsable header

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
 // import logo from "./logo.svg";
 import "./App.css";
+
 // index.js or App.js
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+import Contents from "./components/Contents";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <div className="App">
+        <header>
+          <nav>
+            <Header />
+          </nav>
+        </header>
         <body>
-          Now is the time to test a font!
-          <p>Test test test</p>
-          <div className="test">Test</div>
+          <main>
+            <Contents />
+          </main>
         </body>
-      </div>
-      <div className="container bg-primary">
-        <div className="row">
-          <div className="col">
-            <p>Sebastian</p>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col bg-success">Left Column</div>
-          <div className="col bg-success">Right Column</div>
-        </div>
       </div>
     </>
   );
---
+++ src/components/Contents.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Contents() {
+  return (
+    <div>
+      <p>Contents</p>
+      <div className="table">
+        <div className="row">
+          <div className="col">
+            <p>One</p>
+          </div>
+          <div className="col">
+            <p>Two</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
